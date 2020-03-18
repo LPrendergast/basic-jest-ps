8eb48d8488fd881d31eb07bcdf1856370734486e
Luke Prendergast <luke.prendergast94@gmail.com>
2020-03-18 16:04:50 +0000

    store locator tests button tests, checking if rendering

---
+++ src/containers/StoreLocator.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StoreLocator = () => {
+    return (
+        <div>Test</div>
+    )
+}
+
+export default StoreLocator
--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme'
 import App from '../App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App", () => {
+  it('renders without crashing', () => {
+    let mountedApp = shallow(<App />);
+  });
+
+  it('renders a StoreLocator', () => {
+    let mountedApp = shallow(<App />);
+    const locators = mountedApp.find('StoreLocator')
+    expect(locators.length).toBe(1)
+  });
+
+})
+
